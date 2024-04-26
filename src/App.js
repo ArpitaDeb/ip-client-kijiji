ddd96ead56a264aa9037cd989019f6156f4280f4
Kaiyue Zhou <k@Kaiyues-MacBook-Pro.local>
2024-04-26 01:28:25 -0700

    add form, header & footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import AddNewGroup from "./pages/AddNewGroup/AddNewGroup"
 
 import './styles/partials/_global.scss';
 
@@ -14,6 +15,7 @@ function App() {
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/addNewGroup" element={<AddNewGroup />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/AddNewGroup/AddNewGroup.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import GroupForm from "../../components/GroupForm/GroupForm"
+import Footer from "../../components/Footer/Footer"
+
+
+const AddNewGroup = () => {
+  
+  return (
+    <>
+        <GroupForm />
+        <Footer />
+    </>
+  );
+};
+
+export default AddNewGroup;
