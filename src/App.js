80074d55185b418b155923a7550360f77dc559f1
Adiam Ahmed <adu.adu911@gmail.com>
2024-04-26 10:41:48 -0400

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import AddNewGroup from "./pages/AddNewGroup/AddNewGroup"
-// import GroupProfilePage from './pages/GroupProfilePage/GroupProfilePage'
+import GroupProfilePage from './pages/GroupProfilePage/GroupProfilePage'
+import HomePage from './pages/HomePage/HomePage';
 
 
 import './styles/partials/_global.scss';
@@ -16,9 +16,10 @@ function App() {
           <Navbar />
         </header>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<HomePage />} />
           <Route path="/addNewGroup" element={<AddNewGroup />} />
-          {/* <Route path="/groups/:groupId" element={<GroupProfilePage />} /> */}
+          <Route path="/groups/:groupId" element={<GroupProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
