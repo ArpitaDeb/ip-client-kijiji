51c60e4ad202f4d347ff31e3d3b81fcbf1937c55
Adiam Ahmed <adu.adu911@gmail.com>
2024-04-26 13:50:34 -0400

    corrected mainfest json file

--- src/pages/AddNewGroup/AddNewGroup.jsx
+++ src/pages/AddNewGroup/AddNewGroup.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import GroupForm from "../../components/GroupForm/GroupForm"
-import Footer from "../../components/Footer/Footer"
+
 
 
 const AddNewGroup = () => {
@@ -8,7 +8,7 @@ const AddNewGroup = () => {
   return (
     <>
         <GroupForm />
-        <Footer />
+    
     </>
   );
 };
